f315a0d29b5238c51c592db2eabf5c07ab86d1d7
Osama-Abu-Al-Halaweh <osama.halaweh.0@gmail.com>
2026-03-28 23:01:02 +0300

    project codes and templates

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify, render_template
+from google import genai
+
+app = Flask(__name__)
+
+client = genai.Client(api_key="AIzaSyAPVmG4AgpTsxB6IGIOmWArM1p1FMyu2Jc")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    user_message = request.json["message"]
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=user_message
+    )
+
+    return jsonify({"reply": response.text})
+
+if __name__ == "__main__":
+    app.run(debug=True)
